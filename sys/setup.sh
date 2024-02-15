3d1334b1a854622a8ff194f263405014f400d369
Julius314 <41953174+Julius314@users.noreply.github.com>
2024-02-15 22:44:02 +0100

    Update setup.sh

--- sys/setup.sh
+++ sys/setup.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y libatlas-base-dev libjasper-dev libhdf5-dev python3-opencv nginx libgpiod2
+sudo apt-get install -y libatlas-base-dev libjasper-dev libhdf5-dev python3-opencv nginx
 sudo mv nginx-default /etc/nginx/sites-available/default
 sudo cp bbcam.service /etc/systemd/system/
 sudo systemctl enable bbcam
