8f45f0d1a6fb30f98e6716cb853657bc5b5a959d
Julius314 <41953174+Julius314@users.noreply.github.com>
2024-02-15 23:31:46 +0100

    Update setup.sh

--- sys/setup.sh
+++ sys/setup.sh
@@ -1,4 +1,8 @@
+# install dependencies
 sudo apt-get install -y libatlas-base-dev libjasper-dev libhdf5-dev python3-opencv nginx
-sudo mv nginx-default /etc/nginx/sites-available/default
+# configure nginx to route web traffic to port where flask runs
+sudo cp nginx-default /etc/nginx/sites-available/default
+# configure service
 sudo cp bbcam.service /etc/systemd/system/
+# start service on startup
 sudo systemctl enable bbcam
