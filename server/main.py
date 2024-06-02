b8d2413806140bceac17cafd12ce0fa68da4b29d
Julius314 <41953174+Julius314@users.noreply.github.com>
2024-06-02 22:13:58 +0200

    Update main.py

--- server/main.py
+++ server/main.py
@@ -30,7 +30,7 @@ def gen(camera):
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-	time.sleep(0.1)
+        time.sleep(0.1)
 
 @app.route('/')
 def index():
